26e40d1238f3fb9fc7ec646ef341aa0a0bed6967
Niko Nmaki <nikon.niittymaki@gmail.com>
2026-04-17 14:10:47 +0300

    Eka kommitti

---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts (valinnainen) -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+    <!-- Oma CSS -->
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+        <body class=frontpage-bg>
+            <section>
+            <h1>Terve terve tassa heikki</h1>
+            <section>
+        </body>
+</html>
